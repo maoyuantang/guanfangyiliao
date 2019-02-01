7134448c614ceabed0bca7dcf8cbf162ca6214ab
kexiong <1739944268@qq.com>
2019-02-01 10:55:48 +0800

    '保存代码'

--- src/components/familyMedicine/familyMedicineDoctor.vue
+++ src/components/familyMedicine/familyMedicineDoctor.vue
@@ -1,29 +1,392 @@
 <template>
-	<div class="family-medicine">
+	<div class="family-medicine-doctor">
 		家医系统 医生端
+        <div class="family-medicine-doctor-head">
+            <div class="family-medicine-doctor-head-left">
+                <selftag v-model="dateList" @reback="getDateSelect"></selftag>   
+                <selftag v-model="busModules" @reback="getDateSelect"></selftag>    
+            </div>
+            <div class="family-medicine-doctor-head-right">
+                <publicTime @timeValue="getSelectTime"></publicTime>
+            </div>
+        </div>
+        <div class="family-medicine-doctor-body">
+            <table>
+                <thead class="family-medicine-doctor-thead">
+                    <tr>
+                        <th>{{tableInfo.thead.businessName || 'null'}}</th>
+                        <th>{{tableInfo.thead.modelName || 'null'}}</th>
+                        <th>{{tableInfo.thead.orderTime || 'null'}}</th>
+                        <th>{{tableInfo.thead.userName || 'null'}}</th>
+                        <th>{{tableInfo.thead.userPhone || 'null'}}</th>
+                        <th>{{tableInfo.thead.doctorName || 'null'}}</th>
+                        <th>{{tableInfo.thead.status || 'null'}}</th>
+                        <th>{{tableInfo.thead.orderId || 'null'}}</th>
+                        <th class="family-medicine-doctor-body-spe">{{tableInfo.thead.orderNo || 'null'}}</th>
+                    </tr>
+                </thead>
+                <tbody  class="family-medicine-doctor-body">
+                    <tr v-for="(item,index) in tableInfo.tbody" :key="index">
+                        <th>{{item.businessName || 'null'}}</th>
+                        <th>{{item.modelName || 'null'}}</th>
+                        <th>{{item.orderTime || 'null'}}</th>
+                        <th>{{item.userName || 'null'}}</th>
+                        <th>{{item.userPhone || 'null'}}</th>
+                        <th>{{item.doctorName || 'null'}}</th>
+                        <th>{{item.status || 'null'}}</th>
+                        <th><el-button type="danger" plain size="mini" @click="changeStatus(item,index)">服务变更</el-button></th>
+                        <th class="family-medicine-doctor-body-spe">
+                            <el-button type="warning" size="mini" plain>查看档案</el-button>
+                            <el-button type="success" size="mini" plain>发送</el-button>
+                        </th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <el-dialog
+        :visible.sync="alertInfo.show">
+        <div class="alert-centent">
+            <h1 class="alert-centent-title">服务变更</h1>
+            <div class="alert-centent-item">
+                <span class="alert-centent-item-name">时间:</span>
+                <div>
+                    <el-date-picker
+                    v-model="alertInfo.data.time"
+                    type="date"
+                    size="mini"
+                    value-format="yyyy-MM-dd"
+                    placeholder="选择日期">
+                    </el-date-picker>
+                </div>
+            </div>
+            <div class="alert-centent-item">
+                <span class="alert-centent-item-name">地点:</span>
+                <div>
+                    <el-input
+                    placeholder="请输入内容"
+                    size="mini"
+                    v-model="alertInfo.data.place"
+                    clearable>
+                    </el-input>
+                </div>
+            </div>
+            <div class="alert-centent-submit">
+                <el-button type="primary" @click="submitChange">确定</el-button>
+            </div>
+        </div>
+        <!-- <span>这是一段信息</span>
+        <span>
+            <el-button @click="alertInfo.show = false">取 消</el-button>
+            <el-button type="primary" @click="alertInfo.show = false">确 定</el-button>
+        </span> -->
+        </el-dialog>
 	</div>
 </template>
 
 <script>
+    import { mapState } from 'vuex'
+    import selftag from './../../public/publicComponents/selftag.vue'
+    import publicTime from './../../public/publicComponents/publicTime.vue'
+    import { stencilName, fetchOrderInfo, updateOrderServices } from '../../api/apiAll.js'
 	export default {
+        components:{
+            selftag,
+            publicTime
+        },
+        computed:{
+			...mapState({
+				userInfo:state => state.user.userInfo,
+				userSelfInfo:state => state.user.userSelfInfo
+			})
+		},
 		data () {
 			return {
-				viewCurrent:{
-					
-				}
+                /**
+                 * 顶部日期信息
+                 */
+				dateList:{
+                    more:false,
+                    title:'日期',
+                    list:[
+                        {
+                            text:'全部'
+                        },
+                        {
+                            text:'今日'
+                        }
+                    ]
+                },
+
+                /**
+                 * 顶部 业务模块 信息
+                 */
+                busModules:{
+                    more:false,
+                    title:'业务模块',
+                    list:[
+                        {
+                            text:'全部'
+                        },
+                        {
+                            text:'家庭医生'
+                        },
+                        {
+                            text:'远程监护'
+                        },
+                        {
+                            text:'陪护服务'
+                        },
+                        {
+                            text:'智能陪检'
+                        },
+                        {
+                            text:'在线咨询'
+                        },
+                        {
+                            text:'上门服务'
+                        },
+                        {
+                            text:'家用设备'
+                        }
+                    ]
+                },
+
+                /**
+                 * 表格信息
+                 */
+                tableInfo:{
+                    thead:{},
+                    tbody:[]
+                },
+
+                /**
+                 * 弹窗信息
+                 */
+                alertInfo:{    
+                    show:true,
+                    data:{ 
+                        time:'',
+                        place:''
+                    },
+                    item:null
+                }
 			}
 		},
 		methods:{
-			
+            /**
+             * 初始化接口
+             */
+            initApi(){
+                Promise.all([this.getBusModules(),this.getTableList()])
+            },
+
+			/**
+             * 获取日期选项
+             */
+            getDateSelect(item){
+                console.log(item)
+            },
+
+            /**
+            *  获取用户选择时间
+            *
+            */
+            getSelectTime(time){
+                console.log(time)
+            },
+
+            /**
+             * 17.1获取所有业务模版名
+             */
+            async getBusModules(){
+                const res = await stencilName({token:this.userInfo.token});
+                console.log(res);
+                if(res.data&&res.data.errCode===0){
+                    const modulesMap = [
+                        {
+                            text:'家庭医生',
+                            en:'JTYS'
+                        },
+                        {
+                            text:'上门服务',
+                            en:'SMFW'
+                        },
+                        {
+                            text:'智能陪检',
+                            en:'ZNPJ'
+                        },
+                        {
+                            text:'远程监护',
+                            en:'YCJH'
+                        },
+                        {
+                            text:'在线咨询',
+                            en:'ZXZX'
+                        },
+                        {
+                            text:'家用设备',
+                            en:'JYSB'
+                        },
+                        {
+                            text:'陪护服务',
+                            en:'PHFW'
+                        }
+                    ];
+                    this.busModules.list = res.data.body.map(item=>{
+                        for(const i of modulesMap){
+                            if(item === i.en) return i
+                        }
+                    });
+                    console.log(this.busModules.list)
+                }else{
+                    this.$notify({
+						title: '失败',
+						message: '业务模版获取失败',
+						type: 'error'
+					});
+                }
+            },
+
+            /**
+             * 6.7.家医系统中订单列表（WEB端使用）
+             */
+            async getTableList(){
+                const res = await fetchOrderInfo({token:this.userInfo.token});
+                console.log(res);
+                if(res.data&&res.data.errCode===0){
+                    this.tableInfo.thead = res.data.body.header;
+                    this.tableInfo.tbody = res.data.body.data2.list;
+                    console.log(this.tableInfo)
+                }else{
+                    this.$notify({
+						title: '失败',
+						message: '列表数据获取失败',
+						type: 'error'
+					});
+                }
+            },
+
+            /**
+             * 弹窗被关闭前
+             */
+            handleClose(){
+                console.log('弹窗被关闭了')
+            },
+
+            /**
+             * 服务变更 按钮被点击
+             */
+            changeStatus(item,index){
+                console.log('enter')
+                this.alertInfo.item = item;
+                this.alertInfo.show = true
+                console.log(item)
+            },
+
+            /**
+             * 提交 服务变更
+             */
+            async submitChange(){
+                console.log(this.alertInfo.item)
+                const option = [
+                    {token:this.userInfo.token},
+                    {
+                        orderId:this.alertInfo.item.orderId,
+                        serviceLocation:this.alertInfo.data.place,
+                        serviceTime:this.alertInfo.data.time
+                    }
+                ];
+                console.log(option)
+                const res = await updateOrderServices(...option);
+                console.log(res);
+                if(res.data&&res.data.errCode===0){
+                    this.$notify({
+                        title: '成功',
+                        message: '修改成功',
+                        type: 'success'
+                    });
+                    this.alertInfo = {
+                        show:false,
+                        data:{ 
+                            time:'',
+                            place:''
+                        },
+                        item:null
+                    }
+                }else{
+                    this.$notify({
+						title: '失败',
+						message: '修改失败',
+						type: 'error'
+					});
+                }
+            },
 		},
 		async created(){
-
+            this.initApi();
 		}
 	}
 </script>
 
 <style scoped>
-	.family-medicine{
+	.family-medicine-doctor{
 
+    }
+    .family-medicine-doctor-head{
+        display: flex;
+
+    }
+    .family-medicine-doctor-head>div{
+        flex: 1;
+    }
+    .family-medicine-doctor-body table{
+        width: 100%;
+    }
+    .family-medicine-doctor-body tr{
+        border-bottom: 1px solid #E5EDF3;
+        display: flex;
+    }
+    .family-medicine-doctor-body th{
+        padding-top: 0.1rem;
+        padding-bottom: 0.12rem;
+        flex: 1;
+        text-align: center;
+    }
+    .family-medicine-doctor-body table .family-medicine-doctor-body-spe{
+        flex: 2;
+    }
+    .alert-centent-item{
+        display: flex;
+        align-items: center;
+        padding-top: 0.24rem;
+        
+    }
+    .alert-centent-item > span{
+        flex: 1;
+    }
+    .alert-centent-item > div{
+        flex: 4;
+    }
+    .alert-centent-item .el-input {
+        max-width: 2rem;
+    }
+    .alert-centent-item .el-date-editor{
+        max-width: 2rem;
+    }
+    .alert-centent-title{
+        font-family: var(--fontFamily3);
+        font-size: var(--fontSize7);
+        color: #323C47;
+        letter-spacing: 0;
+        text-align: center;
+    }
+    .alert-centent-submit{
+        padding-top: 0.59rem;
+    }
+    .alert-centent-submit>button{
+        width: 100%;
+    }
+    .alert-centent{
+        padding-left: 0.2rem;
     }
 </style>
---
+++ src/public/publicComponents/publicInfoModule.vue
@@ -0,0 +1,164 @@
+<template>
+	<div class="public-info-module" >
+		<div class="public-info-module-layout">
+            <i class="iconfont public-info-module-icon">&#xe612;</i>
+            <div class="public-info-module-layout-top">
+                <div class="public-info-module-logo"><span class="public-info-module-logo-content">家医</span></div>
+                <div class="public-info-module-Introduction">
+                    <h2 class="public-info-module-name">业务名称占位符</h2>
+                    <div class="public-info-module-Introduction-bottom">
+                        <span>业务类型占位符</span>
+                        <span>GF1254126(业务编号)</span>
+                    </div>
+                </div>
+            </div>
+            <div class="public-info-module-layout-bottom">
+                <table>
+                    <thead class="public-info-module-thead">
+                        <tr>
+                            <th>价格</th>
+                            <th>服务人员</th>
+                            <th>业务人次</th>
+                            <th>总收入</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody class="public-info-module-tbody">
+                        <tr>
+                            <th class="public-info-item-price">¥ 288/月</th>
+                            <th class="public-info-item-num">11</th>
+                            <th class="public-info-item-count"> 总 13 | <span class="public-info-item-count-spe">今 0</span></th>
+                            <th class="public-info-item-total">28,256</th>
+                            <th class="public-info-item-disable">禁用</th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+	</div>
+</template>
+
+<script>
+	export default {
+		components:{
+			
+		},
+		computed:{
+			
+		},
+		data () {
+			return {
+			}
+		},
+		methods:{
+			
+		},
+		async created(){
+		}
+	}
+</script>
+
+<style scoped>
+	.public-info-module{
+        padding: 15px;
+    }
+    .public-info-module-layout{
+        height: 1.69rem;
+        position: relative;
+        padding: 0.1rem;
+        border: 1px solid #E1E8EE;
+        border-radius: 4px;
+    }
+    .public-info-module-icon{
+        position: absolute;
+        top: 10px;
+        right: 10px;
+    }
+    .public-info-module-layout-top{
+        display: flex;
+        align-items: center;
+        border-bottom: 1px solid #E4E8EE;
+        padding-top: 14px;
+        padding-bottom: 16px;
+    }
+    .public-info-module-logo{
+        padding-right: 10px;
+    }
+    .public-info-module-logo-content{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 0.53rem;
+        height: 0.53rem;
+        box-sizing: border-box;
+        border-radius: 50%;
+        font-family: PingFangSC-Regular;
+        font-size: 12px;
+        border: 1px solid #4285F4;
+        color: #4285F4;
+        background: rgba(66,133,244,0.20);
+    }
+    .public-info-module-name{
+        font-family: PingFangSC-Semibold;
+        font-size: 15px;
+        color: #002257;
+        letter-spacing: 0.1px;
+        font-weight: bold;
+    }
+    .public-info-module-Introduction-bottom{
+        font-family: PingFangSC-Regular;
+        font-size: 13px;
+        color: #97A3B4;
+        line-height: 22px;
+        display: flex;
+        justify-content: space-between;
+        padding-top: 10px;
+    }
+    .public-info-module-Introduction{
+        flex: 1;
+    }
+    .public-info-module-layout-bottom{
+        padding-top: 10px;
+    }
+    .public-info-module-layout-bottom>table{
+        width: 100%;
+    }
+    .public-info-module-thead{
+        font-family: PingFangSC-Semibold;
+        font-size: 13px;
+        color: #97A3B4;
+    }
+    .public-info-item-price{
+        font-family: OpenSans-Semibold;
+        font-size: 13px;
+        color: #002257;
+        line-height: 22px;
+    }
+    .public-info-item-num{
+        font-family: OpenSans-Semibold;
+        font-size: 13px;
+        color: #4D7CFE;
+        line-height: 22px;
+    }
+    .public-info-item-count{
+        font-family: PingFangSC-Semibold;
+        font-size: 13px;
+        color: #002257;
+        line-height: 22px;
+    }
+    .public-info-item-count-spe{
+        color: #FE4D97;
+    }
+    .public-info-item-total{
+        font-family: PingFangSC-Semibold;
+        font-size: 13px;
+        color: #4D7CFE;
+        line-height: 22px;
+    }
+    .public-info-item-disable{
+        font-family: PingFangSC-Medium;
+        font-size: 14px;
+        color: #FE4D97;
+        text-align: center;
+    }
+</style>
