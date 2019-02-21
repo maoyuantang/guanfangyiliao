6fc8dd0792ac0bab82edfb110d82aaf079df241f
tangmaoyuan <tangmaoyuan@guanfangyiliao.com>
2019-02-21 19:51:29 +0800

    save

--- src/components/medicalTreatment/medicalTreatmentDoc.vue
+++ src/components/medicalTreatment/medicalTreatmentDoc.vue
@@ -1,15 +1,16 @@
 <template>
 	<div class="medical-treatment-doc">
-		
-		
+		<noData></noData>
 	</div>
 </template>
 
 <script>
 	import normalTab from './../../public/publicComponents/normalTab.vue'
+	import noData from './../../public/publicComponents/noData.vue'
 	export default {
 		components:{
-			normalTab
+			normalTab,
+			noData
 		},
 		data () {
 			return {
