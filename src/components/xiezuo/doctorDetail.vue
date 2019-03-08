e0cdc53f445bd049251e82a3086329ce32b29db2
dayoushiziuw <1051590534@qq.com>
2019-03-08 09:50:01 +0800

    dd

--- src/components/xiezuo/doctorDetail.vue
+++ src/components/xiezuo/doctorDetail.vue
@@ -3,29 +3,43 @@
     <div>
         <ul>
             <li v-for="(text,index) in doctorDetailData" :key="index">
-                <div>
-                    {{imgUrl}}
-                    <img :src="imgUrl+text.userId" />
+
+                <div class="headImg">
+                    <img v-if="text.userHeadId" :src="imgUrl+text.userHeadId" />
+                    <img v-else  src="../../assets/img/publicHeadImg.png" />
+                    
                 </div>
                 <div class="evaluateCont">
                     <h5>{{text.userName}}</h5>
-                    <div>{{text.department}}</div>
+                    <div>{{text.orgName}} {{text.deptName}} </div>
                 </div>
-                <div>
+                <!-- <div>
                     文字
-                </div>
+                </div> -->
             </li>
         </ul>
+        <noData v-show="nodataVisable"></noData>
     </div>
 </template>
 <script>
+import noData from "../../public/publicComponents/noData.vue";
 export default {
+    components: {
+        noData
+    },
     data() {
         return {
-            imgUrl:"https://demo.chuntaoyisheng.com:10002/m/v1/api/hdfs/fs/download"
+            imgUrl:
+                "https://demo.chuntaoyisheng.com:10002/m/v1/api/hdfs/fs/download/",
+            nodataVisable: false
         };
     },
     created() {
+        if (this.doctorDetailData.length > 0) {
+            this.nodataVisable = false;
+        } else {
+            this.nodataVisable = true;
+        }
     },
     props: {
         doctorDetailData: Array
@@ -37,4 +51,14 @@ export default {
 };
 </script>
 <style>
+.headImg {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+}
+.headImg > img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+}
 </style>
