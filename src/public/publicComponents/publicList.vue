e9c2db34c3b18a530e5d8bf6bb0e5e540970564c
tangmaoyuan <tangmaoyuan@guanfangyiliao.com>
2019-04-03 16:28:14 +0800

    save

--- src/public/publicComponents/publicList.vue
+++ src/public/publicComponents/publicList.vue
@@ -168,8 +168,10 @@ padding-left: 0;
     text-overflow: ellipsis;
 }
 .el-table th>.cell, .el-table th div{
-    display: inline !important
+    display: inline !important;
+    white-space: nowrap !important;
 }
+
 
 /* 表格公共组件
  <tableList :tableData="adminTableData" :columns="columns" :tableBtn="tableBtn" :cellColor="cellColor" @cellClickData="cellClickData"> </tableList>
