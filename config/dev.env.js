984e934a34876c3f49c3591a642eb687f67c36b0
dayoushiziuw <1051590534@qq.com>
2019-04-15 14:03:12 +0800

    save code

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,10 +1,11 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
-process.env.IMG_PREFIX = '"https://demo.chuntaoyisheng.com:10002"'
+
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   WSS_PATH: '"wss://demo.chuntaoyisheng.com:10002/chat"',
   HTTPS_PATH: '"https://demo.chuntaoyisheng.com:10002"',
-  // IMG_PREFIX: '"https://demo.chuntaoyisheng.com:10002"'
+  IMG_PREFIX: '"https://demo.chuntaoyisheng.com:10002"'
+
 })
