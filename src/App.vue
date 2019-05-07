72970304e4195449cec6b0a86c238ab89222baed
liushuhua <575502327@qq.com>
2019-05-07 14:12:21 +0800

    fix http://192.168.8.198:8091/browse/GFYLTZ-536

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-    <div id="app">
-        <websocket1 v-if="userState.isLogin" ref="mychild"></websocket1>
-        <!-- <div class='videoAccepclass' v-if='receiveVideoVisable'>
+  <div id="app">
+    <websocket1
+      v-if="userState.isLogin"
+      ref="mychild"
+    ></websocket1>
+    <!-- <div class='videoAccepclass' v-if='receiveVideoVisable'>
                 <div>
                     <h4>{{startVideoName}}邀请你视频</h4>
                     <div>
@@ -9,69 +12,62 @@
                         <button @click="refuseVideo()">拒绝</button>
                     </div>
                 </div>
-        </div> -->
-        <!-- <img src="./assets/logo.png">-->
-        <transition>
-            <router-view class="router-view-class" />
-        </transition>
-
-        
-    </div>
+    </div>-->
+    <!-- <img src="./assets/logo.png">-->
+    <transition>
+      <router-view class="router-view-class"/>
+    </transition>
+  </div>
 </template>
 
 <script>
-import "./video/assets/css/bootstrap.original.css";
-import { mapState } from "vuex";
+import './video/assets/css/bootstrap.original.css'
+import { mapState } from 'vuex'
 
-import websocket from "./common/websocket.js";
+import websocket from './common/websocket.js'
 // import websocket from "./common/websocket.js";
-import "./public/publicCss/base.css";
-import websocket1 from "./common/websocket.vue";
+import './public/publicCss/base.css'
+import websocket1 from './common/websocket.vue'
 export default {
-    name: "App",
-    components: {
-        websocket1
+  name: 'App',
+  components: {
+    websocket1,
+  },
+  data() {
+    return {
+      transitionName: 'slide-left',
+    }
+  },
+  computed: {
+    ...mapState({
+      userState: state => state.user.userInfo,
+      userSelfInfo: state => state.user.userSelfInfo,
+      global: state => state.global,
+    }),
+  },
+  created() {
+    // console.error(process.env.WSS_PATH)
+    if (this.userState.token) {
+      this.lianjie()
+    }
+  },
+  methods: {
+    lianjie: function() {
+      this.$nextTick(ev => {
+        // console.log(this.$refs);
+        // console.log(this.$refs.mychild);
+        this.$refs.mychild.initWebSocket(this.userState.token)
+      })
     },
-    data() {
-        return {
-            transitionName: "slide-left"
-        };
-    },
-    computed: {
-        ...mapState({
-            userState: state => state.user.userInfo,
-            userSelfInfo: state => state.user.userSelfInfo,
-            global: state => state.global
-        })
-    },
-    created() {
-        console.log(this.$store.state)
-        // console.error(process.env.WSS_PATH)
-        if (this.userState.token) {
-            this.lianjie();
-        }
-    },
-    methods: {
-        lianjie: function() {
-            this.$nextTick(ev => {
-                // console.log(this.$refs);
-                // console.log(this.$refs.mychild);
-                this.$refs.mychild.initWebSocket(this.userState.token);
-            });
-        }
-    },
-    watch: {}
-};
+  },
+  watch: {},
+}
 </script>
 
 <style lang="scss">
-[v-cloak] {
-    display: none !important;
-}
-
 #app {
-    display: flex;
-    height: 100%;
-    flex-direction: column;
+  display: flex;
+  height: 100%;
+  flex-direction: column;
 }
 </style>
